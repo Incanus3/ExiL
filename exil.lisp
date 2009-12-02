3d73236f8659a2432a617d3bf7d1ef9582d7e0ba
Jakub Kaláb <jakubkalab@gmail.com>
2009-12-02 14:19:43 +0100

    sablona

--- exil.lisp
+++ exil.lisp
@@ -1,1 +1,40 @@
 (in-package :exil)
+
+(defvar *facts* nil)
+(defvar *templates* nil)
+(defvar *rules* nil)
+
+(defmacro deftemplate (name fileds)
+  "Define rule template"
+
+  )
+
+(defmacro assert (fact)
+  "Add fact into working memory"
+
+  )
+
+(defmacro retract (fact)
+  "Remove fact from working memory"
+
+  )
+
+(defmacro deffacts (facts-list)
+  "Create group of facts to be asserted after (reset)"
+
+  )
+
+(defmacro defrule (rule)
+  "Define rule"
+
+  )
+
+(defun reset ()
+  "Reset the environment"
+
+  )
+
+(defun run ()
+  "Run the infenece engine"
+
+  )
