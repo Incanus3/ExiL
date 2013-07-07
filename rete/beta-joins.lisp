31d1f59d8b9d3b4ecf6c92fa57494362279ac82e
Jakub Kalab <jakubkalab@gmail.com>
2013-07-07 20:59:03 +0200

    split large rete files

--- rete/beta-joins.lisp
+++ rete/beta-joins.lisp
@@ -1,43 +1,6 @@
 (in-package :exil-rete)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; tests holds info about one variable-binding consistency test
-;; current- and previous-field-to-test are object-slot descriptors -
-;; integer positions for simple-facts, slot names for template facts
-
-(defclass test ()
-  ((current-field-to-test
-    :reader current-field :initarg :current-field
-    :initform (error "current-field slot has to be specified"))
-   (previous-condition-number
-    :documentation "tells, how many conditions back i must go"
-    :reader previous-condition :initarg :previous-condition
-    :initform 0)
-   (previous-field-to-test
-    :reader previous-field :initarg :previous-field
-    :initform (error "previous-field slot has to be specified"))))
-
-(defgeneric test-equal-p (test1 test2))
-(defgeneric tests-equal-p (test-list1 test-list2))
-
-(defun make-test (current-field previous-condition previous-field)
-  (make-instance 'test
-                 :current-field current-field
-                 :previous-condition previous-condition
-                 :previous-field previous-field))
-
-(defmethod test-equal-p ((test1 test) (test2 test))
-  (with-accessors ((cf1 current-field) (pc1 previous-condition)
-                   (pf1 previous-field)) test1
-    (with-accessors ((cf2 current-field) (pc2 previous-condition)
-                     (pf2 previous-field)) test2
-      (and (equalp cf1 cf2) (= pc1 pc2) (equalp pf1 pf2)))))
-
-(defmethod tests-equal-p ((test-list1 list) (test-list2 list))
-  (and (= (length test-list1) (length test-list2))
-       (every #'test-equal-p test-list1 test-list2)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; beta-join-node provides testing of variable binding consistency between
 ;; current rule condition (matching wmes stored in alpha memory feeding into
 ;; this node) and previous conditions (matchin tokens stored in beta memory
--- rete/create-alpha-net.lisp
+++ rete/create-alpha-net.lisp
@@ -1,44 +1,5 @@
 (in-package :exil-rete)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defvar *debug-rete* nil)
-
-;; rete stores reference to environment, to be able to call its add-match and
-;; remove-match callbacks
-(defclass rete () ((alpha-top-node :reader alpha-top-node
-                                   :initform (make-instance 'alpha-top-node))
-                   (beta-top-node  :accessor beta-top-node
-                                   :initform (make-instance 'beta-top-node))
-                   (environment :reader environment :initarg :environment)))
-
-(defun make-rete (environment)
-  (make-instance 'rete :environment environment))
-
-;; public methods
-(defgeneric add-wme (rete wme)
-  (:documentation "called when new wme has been added to environment, may
-                   result in add-match or remove-match callback"))
-(defgeneric rem-wme (rete wme)
-  (:documentation "called when wme has been removed from environment, may
-                   result in add-match or remove-match callback"))
-(defgeneric new-production (rete production)
-  (:documentation "creates necessary rete network nodes for new production"))
-(defgeneric remove-production (rete production)
-  (:documentation "removes production from all beta memory nodes"))
-
-(defmethod add-wme ((rete rete) (wme fact))
-  (when *debug-rete*
-    (format t "~%------------------------------------------------------"))
-  (activate (alpha-top-node rete) wme))
-
-(defmethod rem-wme ((rete rete) (wme fact))
-  (when *debug-rete*
-    (format t "~%------------------------------------------------------"))
-  (inactivate (alpha-top-node rete) wme))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ;; private methods
 (defgeneric find-test-node (parent field value)
   (:documentation "finds test node with given tested-field and value
