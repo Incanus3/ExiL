7ff1a7c694fdc909321559b9114436a1bf2f503e
Jakub Kalab <jakubkalab@gmail.com>
2013-11-20 17:24:07 +0100

    refactored pattern matching

--- src/core/pattern-matching.lisp
+++ src/core/pattern-matching.lisp
@@ -20,44 +20,36 @@
 	((variable-p pattern1-atom) (cons pattern1-atom pattern2-atom))
 	((not (equalp pattern1-atom pattern2-atom)) :mismatch)))
 
+(defgeneric atom-matcher (object)
+  (:method ((fact fact))       #'match-fact-atom)
+  (:method ((pattern pattern)) #'match-pattern-atom))
+
 ;; matches either simple or template objects using atom-matcher
 ;; returns list of variable bindings, which may contain the symbol :mismatch
-(defgeneric match-against-pattern%%% (object pattern atom-matcher))
+(defgeneric match-against-pattern% (object pattern atom-matcher))
 
-(defmethod match-against-pattern%%% ((object simple-object) (pattern simple-pattern)
+(defmethod match-against-pattern% ((object simple-object) (pattern simple-pattern)
 				     (atom-matcher function))
   (if (= (length (specifier object)) (length (pattern pattern)))
       (mapcar atom-matcher (specifier object) (pattern pattern))
       (list :mismatch)))
 
-(defmethod match-against-pattern%%% ((object template-object) (pattern template-pattern)
+(defmethod match-against-pattern% ((object template-object) (pattern template-pattern)
 				     (atom-matcher function))
   (if (exil-equal-p (template object) (template pattern))
       (iter (for (slot-name . slot-val) :in (slots pattern))
 	    (collect (funcall atom-matcher (object-slot object slot-name) slot-val)))
       (list :mismatch)))
 
-;; matches either fact or pattern against a pattern
-;; returns list of variable bindings, which may contain the symbol :mismatch
-(defgeneric match-against-pattern%% (object pattern))
-
-;; used by activate-rule (environment/activations) and find-matching-facts (env-backward)
-(defmethod match-against-pattern%% ((fact fact) (pattern pattern))
-  (match-against-pattern%%% fact pattern #'match-fact-atom))
-
-;; used by match-rule-against-goal (env-backward)
-(defmethod match-against-pattern%% ((pattern1 pattern) (pattern2 pattern))
-  (match-against-pattern%%% pattern1 pattern2 #'match-pattern-atom))
-
-;; returns list of variable bindings, which may contain the symbol :mismatch
-(defun match-against-pattern% (fact pattern)
-  (remove-duplicates (delete nil (match-against-pattern%% fact pattern)) :test #'equalp))
+(defun remove-noise (bindings)
+  (remove-duplicates (delete nil bindings) :test #'equalp))
 
 ;; matches object (fact or pattern) against a pattern, returns two values
 ;; 1) whether the object matches the pattern
 ;; 2) variable bindings of the match (or nil when they don't match)
 (defmethod match-against-pattern ((object base-object) (pattern pattern))
-  (let* ((bindings (match-against-pattern% object pattern))
+  (let* ((bindings (remove-noise
+                    (match-against-pattern% object pattern (atom-matcher object))))
 	 (match-var (match-var pattern)))
     (if match-var (push (cons match-var (description object)) bindings))
     (if (and (not (find :mismatch bindings))
