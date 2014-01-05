46766c9d35599c1bafb8bae8a6e54b9298a19f00
Jakub Kalab <jakubkalab@gmail.com>
2014-01-05 01:15:17 +0100

    added reset, step, run, halt, undo and redo buttons removed lispworks menus from windows

--- src/gui.lisp
+++ src/gui.lisp
@@ -12,7 +12,8 @@
                    :callback-type :interface))
   (:default-initargs :title "ExiL Facts"
    :visible-min-height 300
-   :visible-min-width 500))
+   :visible-min-width 500
+   :auto-menus nil))
 
 (defmethod selected-fact ((interface facts-gui))
   (choice-selected-item (fact-list interface)))
@@ -40,7 +41,8 @@
                    :callback-type :interface))
   (:default-initargs :title "ExiL Templates"
    :visible-min-height 300
-   :visible-min-width 500))
+   :visible-min-width 500
+   :auto-menus nil))
 
 (defmethod selected-template ((interface templates-gui))
   (choice-selected-item (template-list interface)))
@@ -68,7 +70,8 @@
                      :callback-type :interface))
   (:default-initargs :title "ExiL Rules"
    :visible-min-height 300
-   :visible-min-width 500))
+   :visible-min-width 500
+   :auto-menus nil))
 
 (defmethod selected-rule ((interface rules-gui))
   (choice-selected-item (rule-list interface)))
@@ -91,7 +94,8 @@
                 :reader agenda-list))
   (:default-initargs :title "ExiL Agenda"
    :visible-min-height 300
-   :visible-min-width 500))
+   :visible-min-width 500
+   :auto-menus nil))
 
 (defmethod update-list ((interface agenda-gui))
   (setf (collection-items (agenda-list interface))
@@ -105,27 +109,55 @@
   ((facts :accessor facts-int)
    (templates :accessor templates-int)
    (rules :accessor rules-int)
-   (agenda :accessor agenda-int))
+   (agenda :accessor agenda-int)
+   (env :accessor env :initarg :env))
   (:panes
    (facts-button push-button
-          :text "Facts"
-          :callback 'show-facts
-          :callback-type :interface)
+                 :text "Facts"
+                 :callback 'show-facts
+                 :callback-type :interface)
    (templates-button push-button
-              :text "Templates"
-              :callback 'show-templates
-              :callback-type :interface)
+                     :text "Templates"
+                     :callback 'show-templates
+                     :callback-type :interface)
    (rules-button push-button
-          :text "Rules"
-          :callback 'show-rules
-          :callback-type :interface)
+                 :text "Rules"
+                 :callback 'show-rules
+                 :callback-type :interface)
    (agenda-button push-button
-           :text "Agenda"
-           :callback 'show-agenda
-           :callback-type :interface))
-  (:layouts
-   (buttons row-layout '(facts-button templates-button rules-button agenda-button)))
-  (:default-initargs :title "ExiL Debug Tools" :auto-menus nil))
+                  :text "Agenda"
+                  :callback 'show-agenda
+                  :callback-type :interface)
+   (reset-button push-button
+                 :text "Reset"
+                 :callback 'reset
+                 :callback-type :interface)
+   (step-button push-button
+                :text "Step"
+                :callback 'step-gui
+                :callback-type :interface)
+   (run-button push-button
+                :text "Run"
+                :callback 'run
+                :callback-type :interface)
+   (halt-button push-button
+                :text "Halt"
+                :callback 'halt
+                :callback-type :interface)
+   (undo-button push-button
+                :text "Undo"
+                :callback 'undo
+                :callback-type :interface)
+   (redo-button push-button
+                :text "Redo"
+                :callback 'redo
+                :callback-type :interface))
+   (:layouts
+    (main-layout column-layout '(window-buttons action-buttons undo-actions))
+    (window-buttons row-layout '(facts-button templates-button rules-button agenda-button))
+    (action-buttons row-layout '(reset-button step-button run-button halt-button))
+    (undo-actions   row-layout '(undo-button redo-button)))
+   (:default-initargs :title "ExiL Debug Tools" :auto-menus nil))
 
 (defmethod show-facts ((gui exil-gui))
   (display (facts-int gui)))
@@ -139,15 +171,33 @@
 (defmethod show-agenda ((gui exil-gui))
   (display (agenda-int gui)))
 
-(defmethod initialize-instance :after ((gui exil-gui) &key environment)
+(defmethod reset ((gui exil-gui))
+  (exil-env:reset-env (env gui)))
+
+(defmethod step-gui ((gui exil-gui))
+  (exil-env:step-env (env gui)))
+
+(defmethod run ((gui exil-gui))
+  (exil-env:run-env (env gui)))
+
+(defmethod halt ((gui exil-gui))
+  (exil-env:halt-env (env gui)))
+
+(defmethod undo ((gui exil-gui))
+  (exil-env:undo (env gui)))
+
+(defmethod redo ((gui exil-gui))
+  (exil-env:redo (env gui)))
+
+(defmethod initialize-instance :after ((gui exil-gui) &key env)
   (with-slots (facts templates rules agenda) gui
-    (setf facts (make-instance 'facts-gui :env environment :main gui)
-          templates (make-instance 'templates-gui :env environment :main gui)
-          rules (make-instance 'rules-gui :env environment :main gui)
-          agenda (make-instance 'agenda-gui :env environment))))
+    (setf facts     (make-instance 'facts-gui     :env env :main gui)
+          templates (make-instance 'templates-gui :env env :main gui)
+          rules     (make-instance 'rules-gui     :env env :main gui)
+          agenda    (make-instance 'agenda-gui    :env env))))
 
 (defmethod make-gui ((env exil-env:environment))
-  (make-instance 'exil-gui :environment env))
+  (make-instance 'exil-gui :env env))
 
 (defmethod update-lists ((gui exil-gui))
   (with-slots (facts templates rules agenda) gui
